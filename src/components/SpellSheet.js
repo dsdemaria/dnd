81efda9ca199ec0513b7d0fadad332518ecdc3e4
Derek DeMaria <demariaderek@gmail.com>
2017-03-13 19:32:07 -0700

    styled spellslot component

--- src/components/SpellSheet.js
+++ src/components/SpellSheet.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import styled from 'styled-components'
 import { Grid } from 'react-bootstrap'
 import SpellSlot from './SpellSlot'
+
+const SpellSlotWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`
 
 const SpellSheet = () => (
   <Grid>
     <h2>SpellSheet</h2>
-    <SpellSlot />
+    <SpellSlotWrapper>
+      <SpellSlot />
+      <SpellSlot />
+      <SpellSlot />
+      <SpellSlot />
+      <SpellSlot />
+      <SpellSlot />
+    </SpellSlotWrapper>
   </Grid>
 )
 
--- src/components/SpellSlot.js
+++ src/components/SpellSlot.js
@@ -1,43 +1,55 @@
 import React from 'react'
 import styled from 'styled-components'
-import { ListGroup } from 'react-bootstrap'
+// import SpellSlotBox from './SpellSlotBox'
 
 const Wrapper = styled.div`
-  border-radius: 1px solid black;
-  width: 32%;
+  border: 1px solid LightGray;
+  border-radius: 3px;
+  width: 30%;
+  padding: 2rem;
+  margin: 0 2% 2% 0;
 `
 const WrapperBody = styled.div`
   display: flex;
   justify-content: space-between;
 `
-const SpellSlotTracker = styled.form`
-  width: 20%;
-  background: navy;
+const SpellList = styled.ul`
+  list-style: none;
+  text-align: left;
+  padding: 0;
+`
+const SpellSlotContainer = styled.div`
   display: flex;
+  justify-content: space-around;
   flex-direction: column;
 `
 const SpellSlotBox = styled.div`
   width: 2rem;
   height: 2rem;
-  background: red;
+  background: FireBrick;
+  border-radius: .25rem;
   margin: 0.5rem;
+
+  &:hover {
+    box-shadow: 0 0 .5rem rgba(178,34,34,.6);
+  }
 `
 
 const SpellSlot = () => (
   <Wrapper>
     <h3>Spell Slot Number</h3>
     <WrapperBody>
-      <ListGroup>
+      <SpellList>
         <li>Spell 1</li>
         <li>Spell 2</li>
         <li>Spell 3</li>
-      </ListGroup>
-      <SpellSlotTracker>
+      </SpellList>
+      <SpellSlotContainer>
         <SpellSlotBox />
         <SpellSlotBox />
         <SpellSlotBox />
         <SpellSlotBox />
-      </SpellSlotTracker>
+      </SpellSlotContainer>
     </WrapperBody>
   </Wrapper>
 )
